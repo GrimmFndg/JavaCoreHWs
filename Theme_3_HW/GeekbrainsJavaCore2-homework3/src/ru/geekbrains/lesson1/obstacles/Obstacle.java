3206e1471a33406c44c390704fd466e8fe9c1095
GrimmFndg <GrimmFndg@gmail.com>
2019-02-05 01:58:15 +0300

    Clear junk

--- Theme_3_HW/GeekbrainsJavaCore2-homework3/src/ru/geekbrains/lesson1/obstacles/Obstacle.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.lesson1.obstacles;
-
-import ru.geekbrains.lesson1.Participant;
-
-public abstract class Obstacle {
-
-    public abstract void doIt(Participant animal);
-}
--- Theme_3_HW/GeekbrainsJavaCore2-homework3/src/ru/geekbrains/lesson2/exceptions/MyException.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.lesson2.exceptions;
-
-public class MyException extends RuntimeException {
-
-    public MyException(String message) {
-        super(message);
-    }
-}
