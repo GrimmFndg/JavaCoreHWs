378dc1b0a6874672d2bf22b67430ffb4aa78d9b4
GrimmFndg <GrimmFndg@gmail.com>
2019-02-12 02:05:18 +0300

    GrimmFndg@gmail.com

---
+++ Theme_6_Methd/MainClass.java
@@ -0,0 +1,41 @@
+package Theme_6_Methd;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class MainClass {
+    public static void main(String[] args) {
+        ServerSocket serv = null;
+        Socket sock = null;
+        try {
+            serv = new ServerSocket(8189);
+            System.out.println("Сервер запущен, ожидаем подключения...");
+            sock = serv.accept();
+            System.out.println("Клиент подключился");
+            Scanner sc = new Scanner(sock.getInputStream());
+            PrintWriter pw = new PrintWriter(sock.getOutputStream());
+            while (true) {
+                String str = sc.nextLine();
+                pw.println("Эхо: " + str);
+                pw.flush();
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка инициализации сервера");
+        } finally {
+            try {
+                serv.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private static String getMassage(){
+      String  msg = new Scanner(System.in).nextLine();
+      return msg;
+    }
+}
